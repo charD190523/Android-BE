3c8fad82f6a97945e60d929f73546a8dbea696e4
test <trisc1910@gmail.com>
2025-05-14 19:32:32 +0700

    GetAllMovie

--- BHD/src/main/java/com/example/bhd/security/SecurityConfig.java
+++ BHD/src/main/java/com/example/bhd/security/SecurityConfig.java
@@ -38,10 +38,11 @@ public class SecurityConfig {
                                 .requestMatchers("/api/auth/**",
                                                  "api/user/get-infor",
                                                  "/api/movie/get-detail",
+                                                 "/api/movie/find-all-by-date",
                                                  "/api/movie/getAll").permitAll()
                                 .requestMatchers("/api/user/update-infor",
-                                                 "/api/movie/find-by-date",
-                                                 "/api/movie/find-all-by-date").hasAnyRole(Role.USER.name(), Role.ADMIN.name())
+                                                 "/api/movie/find-by-date"
+                                                 ).hasAnyRole(Role.USER.name(), Role.ADMIN.name())
                                 .requestMatchers("/api/admin/upload/**").hasRole(Role.ADMIN.name())
                                 .anyRequest().authenticated()
                 )
